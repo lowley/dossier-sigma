184ac714da1c2c1617aeb7bc15a12d70ca695106
olivier SAINT-EVE <osainteve@gmail.com>
2025-08-04 17:42:08 +0200

    refacto: composantisation de CompositeManager en cours

--- app/src/main/java/lorry/folder/items/dossiersigma/data/dataSaver/CompositeManager.kt
+++ app/src/main/java/lorry/folder/items/dossiersigma/data/dataSaver/CompositeManager.kt
@@ -2,14 +2,15 @@ package lorry.folder.items.dossiersigma.data.dataSaver
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import lorry.folder.items.dossiersigma.serviceComponents.ICapsuleComponent
 import java.io.File
 import javax.inject.Inject
 
 class CompositeManager @Inject constructor(
     private val targetPath: String,
     private val useOld: Boolean = false
-) {
-    suspend fun save(element: IElementInComposite) {
+): ICapsuleComponent {
+    override suspend fun save(element: IElementInComposite) {
         val file = File(targetPath)
         if (!file.exists())
             return
@@ -21,7 +22,7 @@ class CompositeManager @Inject constructor(
         }
     }
 
-    suspend fun getComposite(): CompositeData? {
+    override suspend fun getComposite(): CompositeData? {
         val file = File(targetPath)
         if (!file.exists())
             return null
@@ -36,7 +37,7 @@ class CompositeManager @Inject constructor(
     /**
      * lecture à chaque fois de l'info dans le fichier/dossier
      */
-    suspend fun <T> getElement(reader: IElementReader<T>): T? {
+    override suspend fun <T> getElement(reader: IElementReader<T>): T? {
         return withContext(Dispatchers.IO) {
             val file = File(targetPath)
             if (!file.exists())
