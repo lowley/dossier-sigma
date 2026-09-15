eed9a173b4d7a6ac8033ce402e7aa2ef10873fa7
lolveley <osainteve@gmail.com>
2026-09-15 15:56:36 +0200

    corrige pb

--- app/src/main/java/lorry/folder/items/dossiersigma/ui/sigma/SigmaViewModel.kt
+++ app/src/main/java/lorry/folder/items/dossiersigma/ui/sigma/SigmaViewModel.kt
@@ -3,6 +3,7 @@ package lorry.folder.items.dossiersigma.ui.sigma
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.webkit.MimeTypeMap
 import androidx.core.content.FileProvider
 import android.graphics.Bitmap
